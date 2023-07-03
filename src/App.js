dd382ac60c029d8bf7d782356851f65f871123d7
Max Espinosa <maxespinosa@Maxs-MacBook-Air.local>
2023-07-03 10:15:53 -0400

    mapping through array sending props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/Greeting';
+
+const students = ['Andrew', 'Ariana', 'Cihan', 
+'Danny', 'Jennifer','John' ,'Kevin D' , 'Kevin' , 'Lorenzo', 'Max' ,'Pat', 'Sam', 'Yaslin' ]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Props Demo</h1>
+        {students.map(student => <Greeting key={student} name={student} />)}
       </header>
     </div>
   );
